ce2880cacebbf4368ef920ac5a362def3210b32b
Patrik <psirb@myuax.com>
2024-11-01 18:26:03 +0100

    Interfaz UsuarioRepository con un metodo findByUsername que permite buscar al usuario por su nombre. Devuelve un optional que es un contenedor que puede contener o no un valor nulo.
    Extiende de JpaRepository que ya nos proporciona los metodos CRUD.

--- src/main/java/com/ministerio/magia/gestorhechizos/seguridad/UsuarioRepository.java
+++ src/main/java/com/ministerio/magia/gestorhechizos/seguridad/UsuarioRepository.java
@@ -1,4 +1,14 @@
 package com.ministerio.magia.gestorhechizos.seguridad;
 
-public class UsuarioRepository {
+import com.ministerio.magia.gestorhechizos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    // Método para buscar un usuario por su nombre de usuario
+    Optional<Usuario> findByUsername(String username);
 }
