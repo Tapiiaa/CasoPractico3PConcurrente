0c5e6272697a923e6ba82eccc43c9ad228e2b1aa
Patrik <psirb@myuax.com>
2024-11-01 18:50:10 +0100

    Funcion para cargar el usuario por su nombre. Se busca el usuario por su nombre en el repositorio y despues el metodo devuelve el usuario en forma de objeto User del Spring Security. Convierte los roles del usuario.

--- src/main/java/com/ministerio/magia/gestorhechizos/seguridad/SeguridadService.java
+++ src/main/java/com/ministerio/magia/gestorhechizos/seguridad/SeguridadService.java
@@ -5,14 +5,37 @@ import com.ministerio.magia.gestorhechizos.model.Usuario;
 import com.ministerio.magia.gestorhechizos.seguridad.RoleRepository;
 import com.ministerio.magia.gestorhechizos.seguridad.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class SeguridadService {
+public class SeguridadService implements UserDetailsService {
     private final UsuarioRepository usuarioRepository;
     private final RoleRepository roleRepository;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        Usuario usuario = usuarioRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado: " + username));
+        return new org.springframework.security.core.userdetails.User(
+                usuario.getUsername(),
+                usuario.getPassword(),
+                mapRolesToAuthorities(usuario.getRoles())
+        );
+    }
 
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getNombre()))
+                .collect(Collectors.toList());
+    }
 
 }
