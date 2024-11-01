1b43edec8cebb84b6e3a9b3addaefe4a33c9c940
Tapiiaa <ptapilob@myuax.com>
2024-11-01 12:50:00 +0100

    Initial commit

---
+++ src/main/java/com/ministerio/magia/gestorhechizos/GestorHechizosApplication.java
@@ -0,0 +1,13 @@
+package com.ministerio.magia.gestorhechizos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorHechizosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestorHechizosApplication.class, args);
+    }
+
+}
