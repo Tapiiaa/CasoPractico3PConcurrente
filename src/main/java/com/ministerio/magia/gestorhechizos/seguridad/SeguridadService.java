fa16d19330aff892835ed3e0d098e13f9123516b
Patrik <psirb@myuax.com>
2024-11-01 18:45:18 +0100

    Inicializacion de las variables.

--- src/main/java/com/ministerio/magia/gestorhechizos/seguridad/SeguridadService.java
+++ src/main/java/com/ministerio/magia/gestorhechizos/seguridad/SeguridadService.java
@@ -1,5 +1,18 @@
 package com.ministerio.magia.gestorhechizos.seguridad;
 
+import com.ministerio.magia.gestorhechizos.model.Role;
+import com.ministerio.magia.gestorhechizos.model.Usuario;
+import com.ministerio.magia.gestorhechizos.seguridad.RoleRepository;
+import com.ministerio.magia.gestorhechizos.seguridad.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SeguridadService {
+    private final UsuarioRepository usuarioRepository;
+    private final RoleRepository roleRepository;
+
+
 
 }
