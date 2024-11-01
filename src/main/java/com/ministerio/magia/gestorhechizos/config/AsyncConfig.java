05f799a83900414248b2bc2d87dcc70257b41a69
Patrik <psirb@myuax.com>
2024-11-01 17:53:51 +0100

    AsyncConfig. Configura la ejecucion async con @EnableAsync. Configuramos un executor con la configuracion basica.

--- src/main/java/com/ministerio/magia/gestorhechizos/config/AsyncConfig.java
+++ src/main/java/com/ministerio/magia/gestorhechizos/config/AsyncConfig.java
@@ -1,4 +1,31 @@
 package com.ministerio.magia.gestorhechizos.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Slf4j
+@Configuration
+@EnableAsync
 public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        log.info("Configurando el executor para realizar las tareas de forma asíncrona...");
+        // Configuración del executor
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        // Configuracion del pool de hilos.
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("Gestor-Hechizos-");
+
+        executor.initialize();
+        return executor;
+    }
 }
